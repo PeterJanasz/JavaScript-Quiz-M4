ceb043087fb9c7499566ba12ee57098b59450734
PeterJanasz <petertjanasz@gmail.com>
2023-07-12 16:19:50 -0700

    changed file name from quiz to index so it can deploy properly.

--- assets/score.js
+++ assets/score.js
@@ -3,7 +3,8 @@ var scores = document.querySelector(".scores");
 function renderScore() {
     var lastScore = (localStorage.getItem("studentScore"));
     if (lastScore !== null) {
-        document.querySelector(".scores").textContent = lastScore;
+        return;
     } 
+    document.querySelector(".scores").textContent = lastScore;
     console.log(lastScore)
 }
