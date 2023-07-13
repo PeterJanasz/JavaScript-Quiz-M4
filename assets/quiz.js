449f20ac2c9fdd57ca98b9ca27697a01307c2867
PeterJanasz <petertjanasz@gmail.com>
2023-07-12 17:04:10 -0700

    bug fix to link score.html page

--- assets/quiz.js
+++ assets/quiz.js
@@ -161,16 +161,17 @@ function endQuiz(){
 submitBtn.addEventListener("click", function(event) {
   event.preventDefault();
 
-  window.location.href = "file:///Users/peterjanasz/Desktop/bootcamp/challenges/Module-4/score.html"
+  window.location.href="./score.html"
 
   var initials = document.querySelector("input").value;
   var score = timerCount;
-
+  console.log(initials)
+  console.log(score)
 //add initials and score to local storage
 localStorage.setItem("studentInitials", initials);
 localStorage.setItem("studentScore", score);
+renderScore();
 })
-
 
   
 //window.location.href = "file:///Users/peterjanasz/Desktop/bootcamp/challenges/Module-4/score.html"
--- assets/score.js
+++ assets/score.js
@@ -1,10 +1,13 @@
 var scores = document.querySelector(".scores");
 
 function renderScore() {
-    var lastScore = (localStorage.getItem("studentScore"));
+var studentScore = localStorage.getItem("studentScore");
+console.log(studentScore)
+
+    /*var lastScore = (localStorage.getItem("studentScore"));
     if (lastScore !== null) {
         return;
     } 
     document.querySelector(".scores").textContent = lastScore;
-    console.log(lastScore)
+    console.log(lastScore);*/
 }
